9eeaa0a5043e371cb1758e726df102ff7fc8162f
Atul <atul109@live.com>
2025-05-02 17:11:21 +0530

    sec commit by soumya

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const connectDB = require('./src/config/db');
 require('dotenv').config();
 const studentRoutes=require('./src/routes/studentRoutes')
 const app = express();
+const cors=require('cors')
 connectDB();
 
 app.use(express.json());
-
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('API running');
 });
